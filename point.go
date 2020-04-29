e3dcfe1297ca6d998c06b94867ca49b73e4b1bc2
唐丽欣 <tanglixin@tanglixindeMacBook-Pro.local>
2020-04-29 19:16:07 +0800

    指针

--- point.go
+++ point.go
@@ -7,8 +7,18 @@ func main(){
 	var a int = 10
 	var ip *int
 	ip = &a
-	
+
+
 	fmt.Printf("a变量的地址:%x\n",&a)
 	fmt.Printf("ip变量的指针:%x\n",ip)
 	fmt.Printf("*ip变量的指针:%d\n",*ip)
+
+	//空指针
+	var fp *float32
+	if fp != nil {
+		fmt.Println("这不是空指针")
+	}else {
+		fmt.Println("是空指针")
+	}
+
 }
