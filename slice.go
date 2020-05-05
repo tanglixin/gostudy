8c0e3f7d9db8fabb5245861462c0a3bb11589b5b
tanglixin <813750651@qq.com>
2020-05-05 21:28:13 +0800

    切片

--- slice.go
+++ slice.go
@@ -4,6 +4,7 @@ package main
 import "fmt"
 
 func main(){
+
 	//定义一个存放int类型的切片，没有限制长度，底层是一个数组
 	var i [] int
 	//定义一个string类型的切片
@@ -18,4 +19,22 @@ func main(){
 	fmt.Printf("len(i)=%d,cap(i)=%d \n",len(i),cap(i))
 	fmt.Printf("len(s)=%d,cap(s)=%d \n",len(s),cap(s))
 
+
+	//由数组得到切片,基于一个数组切割，左包含右不包含
+	a1 := [...]int{1,3,5,7,9,11,13}
+	a3 := a1[0:4]
+	a4 := a1[:4]
+	a5 := a1[:]
+	a6 := a1[1:5]
+	fmt.Println(a1,a3,a4,a5)
+
+	//切片的容量是指底层数组的容量
+	fmt.Println(len(a3),cap(a3)) //4,7
+	fmt.Println(len(a6),cap(a6)) //4,6
+
+
+	//切片再切片,指向底层的数组
+	a6[1] = 1000
+	a7 := a6[0:]
+	fmt.Println(a7,len(a7),cap(a7))
 }
